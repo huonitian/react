54c2366a47445125225639697463872640dadede
DESKTOP-68NV9C5\huang <huonitian@gmall.com>
2018-09-19 18:10:59 +0800

    test

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import { Form, Card, Input, Button, Icon, Row, Col } from 'antd'
+import './index.less'
+const FormItem = Form.Item;
 
-export default class Login extends React.Component {
+class Login extends React.Component {
 
     render () {
+        const { getFieldDecorator } = this.props.form
         return (
-            <div>
-                this is login pages
+            <div className="login">
+                <Row type="flex" justify="center" className="login_box">
+                    <Col span={5}>
+                        <Card>
+                            <Form>
+                                <FormItem>
+                                    <Input prefix={<Icon type="user" />} placeholder="请输入用户名"/>    
+                                </FormItem>   
+                                <FormItem>
+                                    <Input prefix={<Icon type="lock" />} type="password" placeholder="请输入密码"/>    
+                                </FormItem>
+                                <FormItem>
+                                    <Button block type="primary">登录</Button>   
+                                </FormItem>
+                            </Form>
+                        </Card>
+                    </Col>
+                </Row>    
             </div>    
         )
     }
 
 }
+
+export default Form.create()(Login)
